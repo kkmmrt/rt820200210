ae00c31606eec5f9c633c2df050e5bd7f189e1ea
pychas1 <technikas@gmail.com>
2020-02-10 14:27:08 +0200

    2

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
    TextView zyme1;
    TextView zyme2;
+   EditText laukas1, laukas2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         zyme1 = (TextView)findViewById(R.id.textView);
         zyme2 = (TextView)findViewById(R.id.textView2);
+        laukas1 = (EditText)findViewById((R.id.editText));
+        laukas2 = (EditText)findViewById(R.id.editText2);
     }
 
     public void m1Click (View v){
-        String txt = zyme1.getText().toString();
-        double sk = Double.parseDouble(txt);
-        sk=sk+5;
-        zyme2.setText(Double.toString(sk));
+        String txt1 = laukas1.getText().toString();
+        double sk1 = Double.parseDouble(txt1);
+        String txt2 = laukas2.getText().toString();
+        double sk2 = Double.parseDouble(txt2);
+        double suma = sk1+sk2;
+        zyme2.setText(Double.toString(suma));
     }
 }
